31f3b9709c2abe38801c309c60ed82e6612241f3
firebrowser <103041877+firebrowser@users.noreply.github.com>
2023-01-18 10:06:59 +0100

    interface-graphique
    changement de index.xhtml / form.xhtml et création des beans et relations entre les 2 pages

---
+++ src/beans/IndexBean.java
@@ -0,0 +1,58 @@
+package beans;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import businessLogic.IBusinessLogicController;
+import businessLogic.journeyPoint.Hotel;
+import businessLogic.journeyPoint.TouristicSite;
+import spring.springContainer;
+
+@ManagedBean
+@SessionScoped
+public class IndexBean {
+	
+	private IBusinessLogicController controller;
+	private List<Hotel> hotels;
+
+	public IndexBean() { 
+		
+	}
+	
+	
+	public String startHotels() {
+		hotels = controller.getAllHotels();
+		return "hotels";
+	}
+	
+	public String startCreateYourDreamTrip() {
+		return "form";
+	}
+	
+	public String startHistoricalSites() {
+		controller.getAllHistoricalSites();
+		return "toursticsites";
+	}
+
+
+	public IBusinessLogicController getController() {
+		return controller;
+	}
+
+
+	public void setController(IBusinessLogicController controller) {
+		this.controller = controller;
+	}
+
+
+	public List<Hotel> getHotels() {
+		return hotels;
+	}
+
+
+	public void setHotels(List<Hotel> hotels) {
+		this.hotels = hotels;
+	}
+
+}
+
