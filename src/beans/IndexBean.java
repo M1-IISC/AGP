308060d53e68d06fac0a54a286a9824ac722a202
Benjamin <benjamin.walleth@gmail.com>
2023-01-19 11:26:53 +0100

    Update web pages

--- src/beans/IndexBean.java
+++ src/beans/IndexBean.java
@@ -15,13 +15,11 @@ public class IndexBean {
 	private IBusinessLogicController controller;
 	private List<Hotel> hotels;
 
-	public IndexBean() { 
-		
-	}
+	public IndexBean() {}
 	
 	
 	public String startHotels() {
-		hotels = controller.getAllHotels();
+		//hotels = controller.getAllHotels();
 		return "hotels";
 	}
 	
